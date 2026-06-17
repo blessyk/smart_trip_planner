d6123f04838e71a46d0d55ec5aed04334d617291
blessyk <blessykoshyent@gmail.com>
2026-06-17 10:14:32 +0530

    Info Updates

--- src/components/Utils/api.js
+++ src/components/Utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api/",
 });
 
 // Request interceptor to attach bearer token to every request if present in localStorage
